9801dcdd4463d881c77d95d44832736dc7c9555d
Adi Enzel <adienzel@gmail.com>
2025-04-24 06:27:06 -0400

    backup

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math"
+	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"sort"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -23,11 +24,24 @@ import (
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
-type Metrics struct {
-	Latency   []float64
+var methods = [3]string{"GET", "POST", "PUT"}
+
+const MinBody = 10
+const MaxBody = 200
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+type StatSample struct {
+	LatencyMs float64
 	SentBytes int
 	RecvBytes int
-	Messages  int
+}
+
+type Metrics struct {
+	Latency   []float64
+	SentBytes int64
+	RecvBytes int64
+	Messages  int64
 	mu        sync.Mutex
 }
 
@@ -65,39 +79,52 @@ func stringBufferToResponse(respStr string) (*http.Response, error) {
 	return resp, nil
 }
 
-func (m *Metrics) Record(latency float64, sent int, recv int) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.Latency = append(m.Latency, latency)
-	m.SentBytes += sent
-	m.RecvBytes += recv
-	m.Messages++
-}
+//collect metrix data for processing
+//func (m *Metrics) Record(latency float64, sent int, recv int) {
+//	m.mu.Lock()
+//	defer m.mu.Unlock()
+//	m.Latency = append(m.Latency, latency)
+//	m.SentBytes += sent
+//	m.RecvBytes += recv
+//	m.Messages++
+//}
 
-func (m *Metrics) Stats() (avg, median, stddev, skew float64) {
+func (m *Metrics) Stats() (avg, median, stddev, skew, p95, p99 float64) {
+	m.mu.Lock()
+	values := append([]float64(nil), m.Latency...)
+	m.Latency = nil
+	m.mu.Unlock()
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	n := len(m.Latency)
 	if n == 0 {
 		return
 	}
-	values := append([]float64(nil), m.Latency...)
 	sum := 0.0
 	for _, v := range values {
 		sum += v
 	}
 	avg = sum / float64(n)
-	sorted := append([]float64(nil), values...)
-	sort.Float64s(sorted)
-	median = sorted[n/2]
+	sort.Float64s(values)
+	if n%2 == 0 {
+		median = (values[n/2] + values[n/2+1]) / 2
+	} else {
+		median = values[n/2]
+	}
+	p95 = values[int(0.95*float64(n))]
+	p99 = values[int(0.99*float64(n))-1]
 	var variance float64
+	var s3 float64
 	for _, v := range values {
-		variance += (v - avg) * (v - avg)
+		d := v - avg
+		sd := d * d //power of 2
+		variance += sd
+		s3 += sd * d // power of 3
 	}
 	variance /= float64(n)
 	stddev = math.Sqrt(variance)
-	// skew is rough
-	skew = (avg - median) / stddev
+	g := math.Sqrt(float64(n)) * s3 / math.Pow(variance, 1.5)
+	skew = math.Sqrt(float64(n*(n-1))) * g / float64(n-2)
 	return
 }
 
@@ -106,7 +133,7 @@ func loadTLSConfig(certFile, keyFile, caFile string) (*tls.Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	caCert, err := ioutil.ReadFile(caFile)
+	caCert, err := os.ReadFile(caFile)
 	if err != nil {
 		return nil, err
 	}
@@ -119,34 +146,137 @@ func loadTLSConfig(certFile, keyFile, caFile string) (*tls.Config, error) {
 	}, nil
 }
 
-func startClient(clientID int, metrics *Metrics, tlsConfig *tls.Config, ports []int, messagesPerConn int, msgInterval time.Duration, server string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for _, port := range ports {
-		url := fmt.Sprintf("wss://%s:%d/ws", server, port)
-		conn, _, err := websocket.DefaultDialer.Dial(url, http.Header{"Connection": {"upgrade"}})
-		if err != nil {
-			log.Printf("Client %d: Connection failed to %s: %v", clientID, url, err)
-			continue
-		}
-		for i := 0; i < messagesPerConn; i++ {
-			t0 := time.Now()
-			msg := fmt.Sprintf("client-%d-msg-%d", clientID, i)
-			err := conn.WriteMessage(websocket.TextMessage, []byte(msg))
-			if err != nil {
-				log.Printf("Write failed: %v", err)
-				break
-			}
-			type_, reply, err := conn.ReadMessage()
-			if err != nil || type_ != websocket.TextMessage {
-				log.Printf("Read failed: %v", err)
-				break
-			}
-			latency := float64(time.Since(t0).Milliseconds())
-			metrics.Record(latency, len(msg), len(reply))
-			time.Sleep(msgInterval)
-		}
-		conn.Close()
+func generateRandomAlphanumeric(length int) []byte {
+	b := make([]byte, length)
+	charsetSize := len(charset)
+	for i := range b {
+		b[i] = charset[rand.Intn(charsetSize)]
 	}
+	return b
+}
+
+func clientWorker(mtlsDialer websocket.Dialer,
+	server string,
+	port int,
+	clientID int,
+	messagesPerConn int,
+	msgInterval time.Duration,
+	ch chan StatSample,
+	cwg *sync.WaitGroup) {
+	defer cwg.Done()
+	url := fmt.Sprintf("wss://%s:%d/ws", server, port)
+	conn, _, err := mtlsDialer.Dial(url, http.Header{"Connection": {"upgrade"}})
+	if err != nil {
+		log.Printf("Client %d: Connection failed to %s: %v", clientID, url, err)
+		return
+	}
+	rand.NewSource(time.Now().UnixNano())
+	for i := 0; i < messagesPerConn; i++ {
+		nano := time.Now().UnixNano()
+		t0 := time.Unix(0, nano)
+
+		var body []byte // later i may use empty as well
+		body = generateRandomAlphanumeric(rand.Intn((MaxBody - MinBody + 1) + MinBody))
+		method := methods[rand.Intn(3)]
+		request, err := http.NewRequest(method, fmt.Sprintf("/%d/msgNumber/%d", int64(clientID), i), bytes.NewBuffer(body))
+		request.Header.Add("Content-Type", "application/text")
+		request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+		request.Header.Add("X-Client-ID", strconv.FormatInt(int64(clientID), 10))
+		request.Header.Add("X-Star-Time", strconv.FormatInt(nano, 10))
+
+		msg, err := requestToStringBuffer(request)
+		if err != nil {
+			log.Printf("requestToStringBuffer filed to convert %v", err)
+			break
+		}
+
+		///msg := fmt.Sprintf("client-%d-msg-%d", clientID, i)
+
+		err = conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		if err != nil {
+			log.Printf("Write failed: %v", err)
+			break
+		}
+		type_, reply, err := conn.ReadMessage()
+		if err != nil || type_ != websocket.TextMessage {
+			log.Printf("Read failed: %v", err)
+			break
+		}
+		latency := float64(time.Since(t0).Milliseconds())
+		response, err := stringBufferToResponse(string(reply))
+		if err != nil || type_ != websocket.TextMessage {
+			log.Printf("Failed to convert to http rresponse: %v", err)
+		}
+		bodySize := response.ContentLength
+		if int64(len(reply)) < bodySize {
+			log.Printf("Srange shouldent happend")
+		}
+		ch <- StatSample{
+			LatencyMs: latency,
+			SentBytes: len(msg),
+			RecvBytes: len(reply),
+		}
+		//metrics.Record(latency, len(msg), len(reply))
+		time.Sleep(msgInterval)
+	}
+	err = conn.Close()
+	if err != nil {
+		log.Printf("Client %d: Connection close failed to %s: %v", clientID, url, err)
+	}
+
+}
+
+func startClient(clientID int,
+	ch chan StatSample,
+	tlsConfig *tls.Config,
+	ports []int,
+	messagesPerConn int,
+	msgInterval time.Duration,
+	server string,
+	wg *sync.WaitGroup) {
+	defer wg.Done()
+	// build dialer with the MTLS
+	mtlsDialer := websocket.Dialer{
+		TLSClientConfig: tlsConfig,
+	}
+	var cwg sync.WaitGroup
+	var i int
+	i = 0
+	for _, port := range ports {
+		cwg.Add(i)
+		i++
+		go clientWorker(mtlsDialer, server, port, clientID, messagesPerConn, msgInterval, ch, &cwg)
+		clientID += 1
+		//url := fmt.Sprintf("wss://%s:%d/ws", server, port)
+		//conn, _, err := mtlsDialer.Dial(url, http.Header{"Connection": {"upgrade"}})
+		//if err != nil {
+		//	log.Printf("Client %d: Connection failed to %s: %v", clientID, url, err)
+		//	continue
+		//}
+		//for i := 0; i < messagesPerConn; i++ {
+		//	t0 := time.Now()
+		//	msg := fmt.Sprintf("client-%d-msg-%d", clientID, i)
+		//	err := conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		//	if err != nil {
+		//		log.Printf("Write failed: %v", err)
+		//		break
+		//	}
+		//	type_, reply, err := conn.ReadMessage()
+		//	if err != nil || type_ != websocket.TextMessage {
+		//		log.Printf("Read failed: %v", err)
+		//		break
+		//	}
+		//	latency := float64(time.Since(t0).Milliseconds())
+		//	metrics.Record(latency, len(msg), len(reply))
+		//	time.Sleep(msgInterval)
+		//}
+		//err = conn.Close()
+		//if err != nil {
+		//	log.Printf("Client %d: Connection close failed to %s: %v", clientID, url, err)
+		//	continue
+		//}
+	}
+	cwg.Wait()
 }
 
 func main() {
@@ -162,11 +292,15 @@ func main() {
 	key := os.Getenv("CLIENT_KEY")
 	pushURL := os.Getenv("PROM_PUSHGATEWAY_URL")
 
+	// initialize the list of server ports to use
 	ports := make([]int, portCount)
 	for i := range ports {
 		ports[i] = startPort + i
 	}
+
+	// time to wait between one message to the second one where 0.2 means wait 5 seconds where 5 means 200 milliseconds
 	interval := time.Duration(float64(time.Second) / msgRate)
+	sampleRate := 5
 
 	tlsConfig, err := loadTLSConfig(cert, key, ca)
 	if err != nil {
@@ -174,28 +308,73 @@ func main() {
 	}
 
 	metrics := &Metrics{}
+	statsChan := make(chan StatSample, 10000)
 	var wg sync.WaitGroup
 
 	for i := 0; i < numClients; i++ {
 		wg.Add(1)
-		go startClient(i, metrics, tlsConfig, ports, messagesPerClient, interval, server, &wg)
+		go startClient(i*portCount, statsChan, tlsConfig, ports, messagesPerClient, interval, server, &wg)
 	}
 
+	go func() { // collects stats for later process
+		for sample := range statsChan {
+			metrics.mu.Lock()
+			metrics.Latency = append(metrics.Latency, sample.LatencyMs)
+			metrics.mu.Unlock()
+			atomic.AddInt64(&metrics.SentBytes, int64(sample.SentBytes))
+			atomic.AddInt64(&metrics.RecvBytes, int64(sample.RecvBytes))
+			atomic.AddInt64(&metrics.Messages, 1)
+		}
+	}()
+
 	go func() {
-		tick := time.NewTicker(5 * time.Second)
+		tick := time.NewTicker(time.Duration(sampleRate) * time.Second)
 		for range tick.C {
-			a, m, s, sk := metrics.Stats()
-			log.Printf("Stats: avg=%.2fms med=%.2fms std=%.2f skew=%.2f", a, m, s, sk)
-			pusher := push.New(pushURL, "ws_test").Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-				Name: "avg_latency_ms", Help: "Average latency in ms",
-			}, func() float64 { return a })).
+			msgCount := atomic.SwapInt64(&metrics.Messages, 0)
+			sent := atomic.SwapInt64(&metrics.SentBytes, 0)
+			recv := atomic.SwapInt64(&metrics.RecvBytes, 0)
+
+			msgRate := float64(msgCount) / float64(sampleRate)
+			sentRate := float64(sent) / float64(sampleRate)
+			recviveRate := float64(recv) / float64(sampleRate)
+
+			avg, median, sdv, skew, p95, p99 := metrics.Stats() // get statistics results
+			log.Printf("[%ds] rate=%.1fmsg/s sent=%.1fB/s recv=%.1fB/s avg=%.2fms med=%.2fms std=%.2f skew=%.2f p95=%.2fms p99=%.2fms",
+				sampleRate, msgRate, sentRate, recviveRate, avg, median, sdv, skew, p95, p99)
+
+			err := push.New(pushURL, "ws_test").
 				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-					Name: "message_count", Help: "Total messages sent",
-				}, func() float64 { return float64(metrics.Messages) }))
-			err := pusher.Push()
+					Name: "msg_rate_per_second", Help: "Messages per second",
+				}, func() float64 { return msgRate })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "Average_latency_ms", Help: "Average latency in ms",
+				}, func() float64 { return avg })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "SDV", Help: "SDVs",
+				}, func() float64 { return sdv })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "SKEW", Help: "SKEW",
+				}, func() float64 { return skew })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "median_latency_ms", Help: "median latency in ms",
+				}, func() float64 { return median })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "latency_p95_ms", Help: "95th percentile latency",
+				}, func() float64 { return p95 })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "latency_p99_ms", Help: "99th percentile latency",
+				}, func() float64 { return p99 })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "Bytes sent", Help: "bytes sent from clients per second",
+				}, func() float64 { return sentRate })).
+				Collector(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+					Name: "Bytes Recived", Help: "bytes recived from clients per second",
+				}, func() float64 { return sentRate })).
+				Push()
 			if err != nil {
-				log.Printf("Push failed: %v", err)
+				log.Printf("Prometheus push failed: %v", err)
 			}
+
 		}
 	}()
 
