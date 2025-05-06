e97c0f29d78442f320575b1321dee2633e02c2be
Adi Enzel <adienzel@gmail.com>
2025-05-06 12:45:50 -0400

    add printig to connection fail to web server

--- main.go
+++ main.go
@@ -284,9 +284,9 @@ func clientWorker(mtlsDialer websocket.Dialer,
 			RecvBytes: len(reply),
 		}
 		//metrics.Record(latency, len(msg), len(reply))
-		log.Printf("Loop took without sleap %v", time.Since(t0).Milliseconds())
+		log.Printf("Loop took without sleap %v, for client {}", time.Since(t0).Milliseconds(), clientID)
 		time.Sleep(msgInterval)
-		log.Printf("Loop took with sleap %v", time.Since(t0).Milliseconds())
+		log.Printf("Loop took with sleap %v, for client {}", time.Since(t0).Milliseconds(), clientID)
 	}
 	err = conn.Close()
 	if err != nil {
